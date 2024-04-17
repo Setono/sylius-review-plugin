e1e93ad4cbd8b1a30ae107d45c1122a48c5c5e62
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-17 10:49:06 +0200

    Update review email

--- src/Mailer/ReviewRequestEmailManager.php
+++ src/Mailer/ReviewRequestEmailManager.php
@@ -27,6 +27,7 @@ final class ReviewRequestEmailManager implements ReviewRequestEmailManagerInterf
             Emails::REVIEW_REQUEST,
             [$email],
             [
+                'reviewRequest' => $reviewRequest,
                 'order' => $order,
                 'channel' => $order->getChannel(),
                 'localeCode' => $order->getLocaleCode(),
