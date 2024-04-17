74259acfb993cd1d375688e6c35e1000f38db836
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-17 15:10:44 +0200

    Register an email form type for the review request email if the mail tester plugin is present

--- src/DependencyInjection/SetonoSyliusReviewExtension.php
+++ src/DependencyInjection/SetonoSyliusReviewExtension.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Setono\SyliusReviewPlugin\DependencyInjection;
 
 use Setono\SyliusReviewPlugin\EligibilityChecker\ReviewRequestEligibilityCheckerInterface;
+use Setono\SyliusReviewPlugin\Form\Type\ReviewRequestEmailType;
 use Setono\SyliusReviewPlugin\Mailer\Emails;
 use Setono\SyliusReviewPlugin\Workflow\ReviewRequestWorkflow;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Webmozart\Assert\Assert;
 
 final class SetonoSyliusReviewExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
@@ -39,6 +42,8 @@ final class SetonoSyliusReviewExtension extends AbstractResourceExtension implem
             ->addTag('setono_sylius_review.review_request_eligibility_checker')
         ;
 
+        self::registerEmailFormType($container);
+
         $loader->load('services.xml');
 
         $this->registerResources(
@@ -62,5 +67,28 @@ final class SetonoSyliusReviewExtension extends AbstractResourceExtension implem
                 ],
             ],
         ]);
+    }
+
+    private static function registerEmailFormType(ContainerBuilder $container): void
+    {
+        if (!$container->hasParameter('kernel.bundles')) {
+            return;
+        }
+
+        $bundles = $container->getParameter('kernel.bundles');
+        Assert::isArray($bundles);
+
+        if (!isset($bundles['SynoliaSyliusMailTesterPlugin'])) {
+            return;
+        }
+
+        $container
+            ->setDefinition(
+                'setono_sylius_review.form.type.review_request_email',
+                new Definition(ReviewRequestEmailType::class, ['%setono_sylius_review.model.review_request.class%']),
+            )
+            ->addTag('form.type')
+            ->addTag('app.resolvable_form_type.resolver')
+        ;
     }
 }
---
+++ src/Form/Type/ReviewRequestEmailType.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusReviewPlugin\Form\Type;
+
+use Setono\SyliusReviewPlugin\Mailer\Emails;
+use Setono\SyliusReviewPlugin\Model\ReviewRequestInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Synolia\SyliusMailTesterPlugin\Form\Type\LimitedEntityType;
+use Synolia\SyliusMailTesterPlugin\Resolver\ResolvableFormTypeInterface;
+
+final class ReviewRequestEmailType extends AbstractType implements ResolvableFormTypeInterface
+{
+    public function __construct(private readonly string $reviewRequestClass)
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('reviewRequest', LimitedEntityType::class, [
+            'class' => $this->reviewRequestClass,
+            'choice_label' => static function (ReviewRequestInterface $reviewRequest): string {
+                return sprintf('Review request #%d - Order %s', (int) $reviewRequest->getId(), (string) $reviewRequest->getOrder()?->getNumber());
+            },
+        ]);
+    }
+
+    public function support(string $emailKey): bool
+    {
+        return Emails::REVIEW_REQUEST === $emailKey;
+    }
+
+    public function getCode(): string
+    {
+        return Emails::REVIEW_REQUEST;
+    }
+
+    public function getFormType(string $emailKey): ResolvableFormTypeInterface
+    {
+        return $this;
+    }
+}
