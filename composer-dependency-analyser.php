1dbca450b5d6eff6021ea44210450166c91e192e
Joachim Løvgaard <joachim@loevgaard.dk>
2025-11-28 14:31:06 +0100

    Ignore classes

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,4 +4,8 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreUnknownClasses([
+        \Synolia\SyliusMailTesterPlugin\Form\Type\LimitedEntityType::class,
+        \Synolia\SyliusMailTesterPlugin\Resolver\ResolvableFormTypeInterface::class,
+    ])
 ;
